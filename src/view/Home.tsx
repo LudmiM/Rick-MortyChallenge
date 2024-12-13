45e0d284e56abcdc87b35950dcf1d460c14b6ede
LudmiM <ludmi.m.1109@gmail.com>
2024-12-13 10:30:02 -0300

    Add fech and list character

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
+import CharacterCard from "../components/cardCharacters";
+
 export default function Home() {
+  const [characters, setCharacters] = useState([]); 
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al obtener los personajes");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCharacters(data.results);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
   return (
-    <div>
-      <h2>Bienvenido a la página principal</h2>
-      <p>Este es el contenido de la página de inicio.</p>
+    <div className="characters-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      {characters.map((character) => (
+        <CharacterCard key={character.id} character={character} />
+      ))}
     </div>
   );
 }
