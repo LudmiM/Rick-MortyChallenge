91a6c29608b54245aef7270cb0b02b0a14bc2cb3
LudmiM <ludmi.m.1109@gmail.com>
2024-12-13 11:28:09 -0300

    Add design filter and interface character

--- src/layouts/filters.tsx
+++ src/layouts/filters.tsx
@@ -1,11 +1,88 @@
-import { Outlet } from 'react-router'
+import { useState } from "react";
+import { IoFilter } from "react-icons/io5";
 
-export default function Layout() {
-    return (
-        <>
-            <nav className="nav-links">
-            </nav>
-            <Outlet />
-        </>
-    );
+interface FilterProps {
+  onFilterChange: (filter: { species: string; gender: string; status: string }) => void;
+}
+
+export default function Filters({ onFilterChange }: FilterProps) {
+  const [isMobileFiltersOpen, setIsMobileFiltersOpen] = useState(false);
+  const [selectedSpecies, setSelectedSpecies] = useState<string>("");
+  const [selectedGender, setSelectedGender] = useState<string>(""); 
+  const [selectedStatus, setSelectedStatus] = useState<string>("");
+
+  const handleFilterChange = () => {
+    onFilterChange({
+      species: selectedSpecies,
+      gender: selectedGender,
+      status: selectedStatus,
+    });
+  };
+
+  return (
+    <div className="w-full flex items-center justify-center">
+      <button
+        className="block lg:hidden text-black bg-[#a6cccc] rounded-sm w-11/12"
+        onClick={() => setIsMobileFiltersOpen(!isMobileFiltersOpen)}
+      >
+        <IoFilter /> Filtros
+      </button>
+      
+      <div className={`lg:flex ${isMobileFiltersOpen ? "block" : "hidden"}`}>
+        <div className="mb-4">
+          <label htmlFor="species" className="block text-white">Species</label>
+          <select
+            id="species"
+            className="select-filter"
+            value={selectedSpecies}
+            onChange={(e) => setSelectedSpecies(e.target.value)}
+          >
+            <option value="">Todas las especies</option>
+            <option value="Human">Human</option>
+            <option value="Alien">Alien</option>
+            <option value="Humanoid">Humanoid</option>
+            <option value="Robot">Robot</option>
+          </select>
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="gender" className="block text-white">Genero</label>
+          <select
+            id="gender"
+            className="select-filter"
+            value={selectedGender}
+            onChange={(e) => setSelectedGender(e.target.value)}
+          >
+            <option value="">Todas las especies</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Genderless">Genderless</option>
+            <option value="Unknown">Unknown</option>
+          </select>
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="status" className="block text-white">Estatus</label>
+          <select
+            id="status"
+            className="select-filter"
+            value={selectedStatus}
+            onChange={(e) => setSelectedStatus(e.target.value)}
+          >
+            <option value="">Todos los status</option>
+            <option value="Alive">Alive</option>
+            <option value="Dead">Dead</option>
+            <option value="Unknown">Unknown</option>
+          </select>
+        </div>
+
+        <button
+          onClick={handleFilterChange}
+          className="bg-[#a6cccc] text-black rounded-sm mr-3"
+        >
+          Filtrar
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import CharacterCard from "../components/cardCharacters";
+import { Character } from "../interfacs/characters";
+import Filters from "../layouts/filters";
 
 export default function Home() {
-  const [characters, setCharacters] = useState([]); 
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
@@ -19,11 +21,51 @@ export default function Home() {
         console.log(error.message);
       });
   }, []);
+
+  const [filters, setFilters] = useState({
+    species: "",
+    gender: "",
+    status: ""
+  });
+  const handleFilterChange = (newFilters: { species: string; gender: string; status: string }) => {
+    setFilters(newFilters);
+  };
+
+  useEffect(() => {
+    const fetchFilteredCharacters = async () => {
+      let url = "https://rickandmortyapi.com/api/character?";
+
+      if (filters.species) {
+        url += `species=${filters.species}&`;
+      }
+      if (filters.gender) {
+        url += `gender=${filters.gender}&`;
+      }
+      if (filters.status) {
+        url += `status=${filters.status}&`;
+      }
+
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setCharacters(data.results);
+      } catch (error) {
+        console.log("Error al obtener los personajes:", error);
+      }
+    };
+
+    fetchFilteredCharacters();
+  }, [filters]);
+
+
   return (
-    <div className="characters-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {characters.map((character) => (
-        <CharacterCard key={character.id} character={character} />
-      ))}
-    </div>
+    <>
+      <Filters onFilterChange={handleFilterChange} />
+      <div className="characters-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        {characters.map((character: Character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
+      </div> 
+    </>
   );
 }
